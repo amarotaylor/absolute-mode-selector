9e3ef88d461c1ddca3625832cf138dc12a42d051
amaro@broadinstitute.org <amaro@broadinstitute.org>
2017-12-21 09:49:05 -0500

    updating notes for saving and outputting models

--- wgs_purity_regression_model_scratch.py
+++ wgs_purity_regression_model_scratch.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 from keras.layers import Input, Dense, concatenate, Conv1D, Flatten, Reshape
 from keras.models import Model, Sequential
 import os
-
+import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -16,7 +16,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-
+import copy
 
 def createNumpyDataSet(data, keys, mode_num):
     np_data = Namespace()
@@ -129,7 +129,7 @@ encoder = LabelEncoder()
 
 encoder.fit(train_set.wgd_vec)
 # fix as a ploidy switch removing info about multiple wgds
-train_set.wgd[train_set.wgd_vec > 1] = 1
+train_set.wgd_vec[train_set.wgd_vec > 1] = 1
 encoded_Y = encoder.transform(train_set.wgd_vec)
 
 seed = 7
@@ -148,11 +148,14 @@ def wgd_model():
 
 estimators = []
 estimators.append(('standardize', StandardScaler()))
-estimators.append(('mlp', KerasClassifier(build_fn=wgd_model, epochs=100, batch_size=100, verbose=1)))
-pipeline = Pipeline(estimators)
-pipeline.fit(X,encoded_Y)
-pipeline.predict(test_set.mut_mat)
+estimators.append(('mlp', KerasClassifier(build_fn=wgd_model, epochs=200, batch_size=10, verbose=1)))
+pipeline_wgd = Pipeline(estimators)
+pipeline_wgd.fit(X,encoded_Y)
 
+predict_wgd_test = pipeline_wgd.predict(test_set.mut_mat)
+test_set.wgd_vec[test_set.wgd_vec >1 ] =1
+np.sum(np.squeeze(predict_wgd_test) - np.squeeze(test_set.wgd_vec) == 0)
+#922
 
 #kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
 #results = cross_val_score(pipeline, X, encoded_Y, cv=kfold)
@@ -174,8 +177,6 @@ print('Compiling')
 model_purity.compile(optimizer='Adamax', loss='mean_squared_error')
 print('Training')
 
-model_purity.fit([train_set.mut_mat], train_set.purity_vec, epochs=10, batch_size=128, verbose=2)
-
 model_ploidy = Model(inputs=[mut_input], outputs=predictions_ploidy)
 print('Compiling')
 
@@ -183,3 +184,178 @@ model_ploidy.compile(optimizer='Adamax', loss='mean_squared_error')
 print('Training')
 
 model_ploidy.fit([train_set.mut_mat], train_set.ploidy_vec, epochs=10, batch_size=128, verbose=2)
+
+
+
+model_purity.fit([train_set.mut_mat], train_set.purity_vec, epochs=10, batch_size=128, verbose=2)
+train_purity_predictions = model_purity.predict(train_set.mut_mat)
+train_wgd_predictions = pipeline.predict(train_set.mut_mat)
+train_keys
+
+def createModeClassificationDataSet(data,purity_pred,wgd_pred, keys):
+    #np_data = Namespace()
+    #sample_num = len(keys)
+    #np_data.mut_mat = np.zeros([sample_num, 301])
+    #np_data.abs_mat = np.zeros([sample_num, len(data.modes_dict[list(keys)[0]].columns) - 1, mode_num])
+    #np_data.purity_vec = np.zeros(sample_num)
+    #np_data.wgd_vec = np.zeros(sample_num)
+    #np_data.solution_idx = np.zeros(sample_num)
+    for i, key in enumerate(keys):
+        x_new = data.modes_dict[key]
+        pred_purity = np.zeros(len(x_new)) + purity_pred[i]
+        pred_wgd = np.zeros(len(x_new)) + wgd_pred[i]
+        x_new['pred_purity'] = pred_purity
+        x_new['pred_wgd'] = pred_wgd
+        x_new['purity_diff'] = np.abs(x_new['pred_purity'] - x_new['alpha'])
+        if i == 0:
+            X_modes = x_new
+        else:
+            X_modes = pd.concat([X_modes,x_new])
+    X_modes.dropna(axis=1, how='all', inplace=True)
+    Y = X_modes['solution_class']
+    Y_array = Y.as_matrix()
+    X_modes.drop('solution_class', axis=1,inplace=True)
+    X_array = X_modes.reset_index().values
+    return X_array, Y_array
+
+[X_array,Y_array] = createModeClassificationDataSet(all_data,train_purity_predictions,train_wgd_predictions, train_keys)
+# encode class values as integers
+encoder = LabelEncoder()
+
+encoder.fit(Y_array)
+# fix as a ploidy switch removing info about multiple wgds
+solution_class = encoder.transform(Y_array)
+def solution_classifier():
+    # create model
+    model = Sequential()
+    model.add(Dense(23, input_dim=23, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(15, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(5, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasClassifier(build_fn=solution_classifier, epochs=200, batch_size=10, verbose=1)))
+pipeline_solutions = Pipeline(estimators)
+pipeline_solutions.fit(X,solution_class)
+
+
+test_purity_predictions = model_purity.predict(test_set.mut_mat)
+test_wgd_predictions = pipeline_wgd.predict(test_set.mut_mat)
+
+def EvaluateSolutionsPicker(data,purity_pred,wgd_pred, keys):
+    picked_correct = 0
+    did_not_pick = 0
+    picked_wrong = 0
+    wrong_idxs = list()
+    solutions_picked = list()
+    for i, key in enumerate(keys):
+        x_test = copy.deepcopy(data.modes_dict[key])
+        x_test['pred_purity'] = np.zeros(len(x_test))+purity_pred[i]
+        x_test['pred_wgd'] = np.zeros(len(x_test))+wgd_pred[i]
+        x_test['purity_diff'] = np.abs(x_test['pred_purity'] - x_test['alpha'])
+        x_test.dropna(axis=1,how='all',inplace=True)
+        solution_test = x_test['solution_class'].as_matrix()
+        x_test.drop('solution_class',axis=1,inplace=True)
+        x_test_array = x_test.reset_index().values
+        solution_array = pipeline_solutions.predict(x_test_array)
+        true_vals = [i for i, x in enumerate(solution_array) if x]
+        if np.sum(solution_test[true_vals]) > 0:
+            picked_correct = picked_correct+1
+            solutions_picked.append(np.max(true_vals)+1)
+        elif np.sum(solution_array) == 0:
+            did_not_pick = did_not_pick+1
+        elif np.sum(solution_array) > 0:
+            wrong_idxs.append(key)
+            picked_wrong = picked_wrong + 1
+    print 'returning'
+    print wrong_idxs
+    return np.true_divide(picked_correct,len(purity_pred)), np.true_divide(did_not_pick,len(purity_pred)), np.true_divide(picked_wrong,len(purity_pred)),solutions_picked
+
+
+[correct_frac,did_not_pick_frac,picked_wrong_frac,solutions_picked] = EvaluateSolutionsPicker(all_data,test_purity_predictions,test_wgd_predictions,test_keys)
+
+
+def manual_review(key,test_keys):
+    i= test_keys.index(key)
+    x_test = copy.deepcopy(all_data.modes_dict[key])
+    x_test['pred_purity'] = np.zeros(len(x_test))+test_purity_predictions[i]
+    x_test['pred_wgd'] = np.zeros(len(x_test))+test_wgd_predictions[i]
+    x_test['purity_diff'] = np.abs(x_test['pred_purity'] - x_test['alpha'])
+    x_test.dropna(axis=1,how='all',inplace=True)
+    solution_test = x_test['solution_class'].as_matrix()
+    #x_test.drop('solution_class',axis=1,inplace=True)
+    x_test_array = x_test.drop('solution_class',axis=1).reset_index().values
+    solution_array = pipeline_solutions.predict(x_test_array)
+    true_vals = [i for i, x in enumerate(solution_array) if x]
+    print x_test
+    print solution_array
+
+
+key = 'CESC-C5-A1MJ-TP-NB'
+manual_review(key, test_keys)
+
+picked_correct = 0
+did_not_pick = 0
+wrong_idxs = list()
+if np.sum(solution_test[true_vals]) > 0:
+    picked_correct = picked_correct + 1
+elif np.sum(solution_test) == 0:
+    did_not_pick = did_not_pick + 1
+elif np.sum(solution_test) > 0:
+    wrong_idxs.append(key)
+
+
+model_json = pipeline_wgd.named_steps['mlp'].model.to_json()
+with open("model_wgd.json", "w") as json_file:
+ json_file.write(model_json)
+
+pipeline_wgd.named_steps['mlp'].model.save_weights('wgd_weights.h5')
+
+model_json = pipeline_solutions.named_steps['mlp'].model.to_json()
+with open("model_solutions.json", "w") as json_file:
+ json_file.write(model_json)
+
+pipeline_solutions.named_steps['mlp'].model.save_weights('solutions_weights.h5')
+
+
+model_json = model_purity.to_json()
+with open("model_purity.json", "w") as json_file:
+ json_file.write(model_json)
+
+model_purity.save_weights('purity_weights.h5')
+
+
+
+# save and load model
+json_file = open('wgd_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("wgd_weights.h5")
+print("Loaded model from disk")
+
+
+
+# save and load model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("wgd_weights.h5")
+print("Loaded model from disk")
+
+
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+true_p = 0
+list_of_solutions = list()
+for i, key in enumerate(test_keys):
+    idx=np.argmin(np.abs(all_data.modes_dict[key]['alpha'] - test_purity_predictions[i]))
+    if all_data.modes_dict[key]['solution_class'][idx] == 1:
+        true_p = true_p + 1
+    list_of_solutions.append(idx+1)
